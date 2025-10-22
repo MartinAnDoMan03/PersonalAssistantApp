1b5c16cb0b591c8a253783d927590d9902d05c35
MartinAnDoMan03 <martinadmanalu@gmail.com>
2025-10-22 23:12:44 +0700

    login register link

--- app/src/main/java/com/example/yourassistantityora/LoginActivity.kt
+++ app/src/main/java/com/example/yourassistantityora/LoginActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,5 +16,12 @@ class LoginActivity : AppCompatActivity() {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
+
+        val goToRegister = findViewById<TextView>(R.id.text_signup_link)
+        goToRegister.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
--- app/src/main/java/com/example/yourassistantityora/RegisterActivity.kt
+++ app/src/main/java/com/example/yourassistantityora/RegisterActivity.kt
@@ -4,11 +4,21 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.yourassistantyora.R
+import android.widget.TextView
+import android.content.Intent
+import com.example.yourassistantyora.LoginActivity
+
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_register)
+
+        val goToLogin = findViewById<TextView>(R.id.text_login_link)
+        goToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
