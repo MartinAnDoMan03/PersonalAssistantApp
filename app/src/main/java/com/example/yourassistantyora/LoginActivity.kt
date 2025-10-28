c7b14b031ac1dbf2444552104511e9b74819e040
Fahri <fahrialhamda31@gmail.com>
2025-10-28 10:16:34 +0700

    fix dikit

--- app/src/main/java/com/example/yourassistantyora/LoginActivity.kt
+++ app/src/main/java/com/example/yourassistantyora/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.yourassistantyora
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.yourassistantyora.ui.theme.YourAssistantYoraTheme
 
@@ -11,7 +10,6 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
 
         setContent {
             YourAssistantYoraTheme {
